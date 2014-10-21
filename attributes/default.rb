83562d4af62546d2fb06e78582ceffcb98927f72
Artem Ivantsov <aivantsov@qubell.com>
2014-10-21 13:07:02 +0300

    test empty attrs

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['cookbook-qubell-build']['dest_path'] = "/tmp"
 default['cookbook-qubell-build']['dest_name'] = "ROOT"
 default['cookbook-qubell-build']['target'] = "/tmp/mvn"
 default['cookbook-qubell-build']['artifacts'] = []
+default['cookbook-qubell-build']['test'] = ''
